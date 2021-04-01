a490929747b295ce3031e9679c37ced8f6c68d20
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2021-04-01 22:09:55 +0530

    images issue fixed

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,12 +1,12 @@
-var historyContent = document.querySelector('#history-content');
-var m3 = document.getElementsByClassName('.our-history');
+// var historyContent = document.querySelector('#history-content');
+// var m3 = document.getElementsByClassName('.our-history');
 
-document.addEventListener('scroll', () => {
-    if (window.scrollY >= historyContent.getBoundingClientRect().top) {
-        historyContent.classList.add('opacity');
-        historyContent.classList.add('fromLeftToRight-animation');
-    }
-})
+// document.addEventListener('scroll', () => {
+//     if (window.scrollY >= historyContent.getBoundingClientRect().top) {
+//         historyContent.classList.add('opacity');
+//         historyContent.classList.add('fromLeftToRight-animation');
+//     }
+// })
 
 
 // Toggle Bars Switch
