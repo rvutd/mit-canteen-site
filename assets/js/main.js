97ef1aa69ab647880d9c928123ce89e4e71a5c0a
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2021-04-04 00:22:46 +0530

    added JSON file

---
+++ assets/js/main.js
@@ -0,0 +1,303 @@
+menu = {
+    "items": [
+        {
+            "sys": { "id": "1" },
+            "fields": {
+                "title": "Double Cheese Potato Burger",
+                "category": "Burger",
+                "price": "45",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "2" },
+            "fields": {
+                "title": "Cheese Sandwitch",
+                "category": "Sandwitch",
+                "price": "45",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "3" },
+            "fields": {
+                "title": "Veg Club Sandwitch",
+                "category": "Sandwitch",
+                "price": "60",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "4" },
+            "fields": {
+                "title": "Cheese Masala Sandwitch",
+                "category": "Sandwitch",
+                "price": "45",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "5" },
+            "fields": {
+                "title": "Veg Schezuan Sandwitch",
+                "category": "Sandwitch",
+                "price": "45",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "6" },
+            "fields": {
+                "title": "Masala Maggie",
+                "category": "Maggie",
+                "price": "25",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "7" },
+            "fields": {
+                "title": "Schezuan Maggie",
+                "category": "Maggie",
+                "price": "30",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "8" },
+            "fields": {
+                "title": "Veg Maggie",
+                "category": "Maggie",
+                "price": "30",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "9" },
+            "fields": {
+                "title": "Cheese Garlic Maggie",
+                "category": "Maggie",
+                "price": "40",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "10" },
+            "fields": {
+                "title": "Cheese Veg Maggie",
+                "category": "Maggie",
+                "price": "45",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "11" },
+            "fields": {
+                "title": "Masala Fries",
+                "category": "Fries",
+                "price": "35",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "12" },
+            "fields": {
+                "title": "Schezuan Fries",
+                "category": "Fries",
+                "price": "45",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "13" },
+            "fields": {
+                "title": "Cheese Fries",
+                "category": "Fries",
+                "price": "40",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "14" },
+            "fields": {
+                "title": "Red Sause Pasta",
+                "category": "Pasta",
+                "price": "80",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "15" },
+            "fields": {
+                "title": "White Sause Pasta",
+                "category": "Pasta",
+                "price": "80",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "16" },
+            "fields": {
+                "title": "Milk Shakes",
+                "category": "Beverages",
+                "price": "35",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "17" },
+            "fields": {
+                "title": "Hot Chocolate",
+                "category": "Beverages",
+                "price": "35",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "18" },
+            "fields": {
+                "title": "Aerated Drinks",
+                "category": "Beverages",
+                "price": "10",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "19" },
+            "fields": {
+                "title": "Cold Coffee",
+                "category": "Beverages",
+                "price": "35",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "20" },
+            "fields": {
+                "title": "Coffee",
+                "category": "Beverages",
+                "price": "15",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "21" },
+            "fields": {
+                "title": "Tea",
+                "category": "Beverages",
+                "price": "10",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "22" },
+            "fields": {
+                "title": "Chocate Frappe",
+                "category": "Beverages",
+                "price": "35",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "23" },
+            "fields": {
+                "title": "Veg Puff",
+                "category": "Bakery",
+                "price": "35",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "24" },
+            "fields": {
+                "title": "Panner Puff",
+                "category": "Bakery",
+                "price": "15",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "25" },
+            "fields": {
+                "title": "Khari",
+                "category": "Bakery",
+                "price": "20",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        },
+        {
+            "sys": { "id": "26" },
+            "fields": {
+                "title": "Noodle Puff",
+                "category": "Bakery",
+                "price": "15",
+                "caleories": "220 - 280 Kcal",
+                "image": { "fields": {"file": { "url": "assets/images/sandwich1.jpg" } } }
+            }
+        }
+    ]
+}
+
+const menuSection = document.querySelector('.menu-section');
+
+window.addEventListener('DOMContentLoaded', () =>{
+    displayMenuItems(menu)
+})
+
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.items.map(function(item){
+      return `
+        <article class="menu-item">
+            <img src="${item.fields.image.fields.file.url}" alt="Product image">
+            <div class="item-info">
+            <figure>
+                <h2>${item.fields.title}</h2>
+                <div class="item-category">${item.fields.category}</div>
+                <div class="flex">
+                    <i class="fas fa-fire"></i>
+                    <p>${item.fields.caleories}</p>
+                </div>
+            </figure>
+            <hr style="margin: 10px 0;">
+            <div class="menu-cart-functionality">
+                <div class="price">$${item.fields.price}</div>
+                <div class="cart-btn-container">
+                    <button><i  class="fas fa-minus"></i></button>
+                    <div>0</div>
+                    <button><i  class="fas fa-plus"></i></button>
+                </div>
+            </div>
+            </div>
+        </article>
+      `;
+    });
+
+    displayMenu = displayMenu.join('');
+    menuSection.innerHTML = displayMenu;
+  }
