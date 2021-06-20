86d03eb22a259bc561b392732d907637e474d01c
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2021-06-20 15:30:35 +0530

    user_footer

--- assets/js/features.js
+++ assets/js/features.js
@@ -245,7 +245,7 @@ if (signModal) {
 `;
 }
 
-// Customer Footer
+// Home/Vision Footer
 var customerFooter = document.querySelectorAll('.customer-footer');
 function customerFooterUI() {
     if (customerFooter){
@@ -300,7 +300,60 @@ function customerFooterUI() {
 }
 customerFooterUI()
 
-let indexPage = encodeURI('index.html')
+// Authenticated User Footer
+var userFooter = document.querySelectorAll('#user-footer');
+function userFooterUI() {
+    if (userFooter){
+        userFooter.forEach(element => {
+            element.innerHTML = `
+            <section class="container-min">
+            <!-- Upper Footer -->
+                <main class="grid grid-3">
+                    <!-- About College -->
+                    <article class="about">
+                        <h1 class="sm">about</h1>
+                        <p>Malwa Institute of Technology is governed by Indus 
+                        Global Educational & Welfare Society which was founded 
+                        in 2003 by a group of industrialists and educationalists
+                        who shared a vision of establishing centers of scientific
+                        learning around the country.</p>
+                        <ul class="py-1">
+                            <li><i class="fab fa-facebook-f"></i><a href="https://www.facebook.com/malwainstitute/" target="_blank"> Facebook</a></li>
+                            <li><i class="fab fa-instagram"></i><a href="https://www.instagram.com/malwa_institute_of_technology/" target="_blank">Instagram</a></li>
+                        </ul>
+                    </article>
+                    <!-- Navigation Links -->
+                    <article class="quick-links">
+                        <ul>
+                            <h1 class="sm">Quick Links</h1>
+                            <li><a href="index.html">Menu</a></li>
+                            <li><a href="our-vision.html">Your Orders</a></li>
+                            <li><a href="https://github.com/rvutd/mit-canteen-site" target="_blank">Git Repository</a></li>
+                            <li><a href="https://mitindore.co.in/" target="_blank">MIT Indore</a></li>
+                        </ul>
+                    </article>
+                    <!-- Contact Details -->
+                    <article class="contact">
+                        <ul>
+                            <h1 class="sm">Contact Information</h1>
+                            <li> <i class="fas fa-phone-alt"></i> <p>+91-731-2810001-08</p></li>
+                            <li> <i class="fas fa-envelope"></i> <p>admission@mitindore.co.in</p></li>
+                            <li> <i class="fas fa-map-marker-alt"></i> <p>Indore-Dewas Bypass Road, Indore (MP)-452016</p></li>
+                            <li> <i class="fas fa-globe-asia"></i> <p>For Professional Courses, Visit: www.altius.ac.in</p></li>
+                        </ul>
+                    </article>
+                </main>
+                <!-- Deep Devs Info -->
+                <main class="deep-devs flex">
+                    <div>© Copyright 2020 - 2021. All Rights Reserved</div>
+                    <div class="">Designed & Developed with <i class="fas fa-heart clr-red"></i> By <a href="#">Deep Devs</a></div>
+                </main>
+            </section>
+        `;
+        })
+    }
+}
+userFooterUI()
 
 // Client Orders Detals
 const showOrderDetails = document.querySelectorAll('#show-order-details');
@@ -469,4 +522,3 @@ function setOrderDetails(trimedEmailID){
         }
     })
 }
-
